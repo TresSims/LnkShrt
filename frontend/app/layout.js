40ebbb56766bfbcce7cb97b271cc5f9a6db5a200
Tres Sims <tres.sim95@gmail.com>
2024-03-25 15:34:21 -0400

    Moved query provider to top level

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import QueryRoot from "./components/QueryRoot";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,41 +14,43 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex w-screen h-screen justify-center place-items-center">
-          <div className="bg-slate-900 w-8/12 rounded-md flex flex-col overflow-hidden">
-            <div className="flex flex-row place-content-between text-lg font-black text-orange-500">
-              <div className="flex flex-row ">
-                <Link
-                  href="/"
-                  className="m-0 p-5 no-underline bg-gradient-to-t to-50% from-blue-500 hover:from-blue-400 active:from-blue-600 border-r-2"
-                >
-                  Home
-                </Link>
-                <Link
-                  href="/remove"
-                  className="m-0 p-5 no-underline bg-gradient-to-t to-50% from-blue-500 hover:from-blue-400 active:from-blue-600 border-r-2"
-                >
-                  Link List
-                </Link>
+        <QueryRoot>
+          <div className="flex w-screen h-screen justify-center place-items-center">
+            <div className="bg-slate-900 w-8/12 rounded-md flex flex-col overflow-hidden">
+              <div className="flex flex-row place-content-between text-lg font-black text-orange-500">
+                <div className="flex flex-row ">
+                  <Link
+                    href="/"
+                    className="m-0 p-5 no-underline bg-gradient-to-t to-50% from-blue-500 hover:from-blue-400 active:from-blue-600 border-r-2"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    href="/remove"
+                    className="m-0 p-5 no-underline bg-gradient-to-t to-50% from-blue-500 hover:from-blue-400 active:from-blue-600 border-r-2"
+                  >
+                    Link List
+                  </Link>
+                </div>
+                <div className="flex">
+                  <Link
+                    href="/account"
+                    className="m-0 p-5 no-underline bg-gradient-to-t to-50% from-blue-500 hover:from-blue-400 active:from-blue-600 border-l-2"
+                  >
+                    Login
+                  </Link>
+                </div>
               </div>
-              <div className="flex">
-                <Link
-                  href="/account"
-                  className="m-0 p-5 no-underline bg-gradient-to-t to-50% from-blue-500 hover:from-blue-400 active:from-blue-600 border-l-2"
-                >
-                  Login
-                </Link>
+              <div className="flex flex-col p-5 justify-center place-items-center w-full">
+                <h1 className="text-5xl py-50 my-5">LnkShrt_</h1>
+                <h2 className="text-2xl italic text-slate-700">
+                  Yet another link shortener
+                </h2>
+                {children}
               </div>
-            </div>
-            <div className="flex flex-col p-5 justify-center place-items-center w-full">
-              <h1 className="text-5xl py-50 my-5">LnkShrt_</h1>
-              <h2 className="text-2xl italic text-slate-700">
-                Yet another link shortener
-              </h2>
-              {children}
             </div>
           </div>
-        </div>
+        </QueryRoot>
       </body>
     </html>
   );
--- frontend/app/remove/layout.js
+++ frontend/app/remove/layout.js
@@ -1,15 +1,9 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 export default function RemoveLayout({ children }) {
-  const queryClient = new QueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="bg-slate-700 p-2 text-black w-full rounded-md">
-        {children}
-      </div>
-    </QueryClientProvider>
+    <div className="bg-slate-700 p-2 text-black w-full rounded-md">
+      {children}
+    </div>
   );
 }
