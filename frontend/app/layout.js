99d375d09c19ee46043a0887c4fcfc7bf1a6353c
Tres Sims <tres.sim95@gmail.com>
2024-04-09 21:05:51 -0400

    Nav interface cleanup

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -34,22 +34,12 @@ export default function RootLayout({ children }) {
                   </Link>
                 </div>
                 <div className="flex">
-                  {!Cookies.get("loggedin") && (
-                    <Link
-                      href="/login"
-                      className="m-0 p-5 no-underline bg-gradient-to-t to-50% from-blue-500 hover:from-blue-400 active:from-blue-600 border-l-2"
-                    >
-                      Signup or login
-                    </Link>
-                  )}
-                  {Cookies.get("loggedin") && (
-                    <Link
-                      href="/manageAccount"
-                      className="m-0 p-5 no-underline bg-gradient-to-t to-50% from-blue-500 hover:from-blue-400 active:from-blue-600 border-l-2"
-                    >
-                      Manage Account
-                    </Link>
-                  )}
+                  <Link
+                    href="/login"
+                    className="m-0 p-5 no-underline bg-gradient-to-t to-50% from-blue-500 hover:from-blue-400 active:from-blue-600 border-l-2"
+                  >
+                    Account
+                  </Link>
                 </div>
               </div>
               <div className="flex flex-col p-5 justify-center place-items-center w-full">
